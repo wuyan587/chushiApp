093475ab53534393e090dbbc05a69dbd72b83fc5
muhongjiu <1612930123@qq.com>
2019-12-26 20:51:26 +0800

    登录注册组件更新

--- src/routers/routerTable.js
+++ src/routers/routerTable.js
@@ -1,25 +1,43 @@
-const Home=()=>import(/* 首页 */ '../views/home/index.vue')
-const Sort=()=>import(/* 分类 */ '../views/sort/index.vue')
-const Release=()=>import(/* 发布 */ '../views/release/index.vue')
-const Shopcart=()=>import(/* 货单 */ '../views/shopcart/index.vue')
-const Mine=()=>import(/* 我的 */ '../views/mine/index.vue')
+const Home = () =>
+    import ( /* 首页 */ '../views/home/index.vue')
+const Sort = () =>
+    import ( /* 分类 */ '../views/sort/index.vue')
+const Release = () =>
+    import ( /* 发布 */ '../views/release/index.vue')
+const Shopcart = () =>
+    import ( /* 货单 */ '../views/shopcart/index.vue')
+const Mine = () =>
+    import ( /* 我的 */ '../views/mine/index.vue')
+const Login = () =>
+    import ( /* 登录*/ '../views/login/index.vue')
+const Register = () =>
+    import ( /* 注册 */ '../views/register/index.vue')
 
-const routerTable=[
+
+
+const routerTable = [{
+        path: '/home',
+        component: Home
+    }, {
+        path: '/sort',
+        component: Sort
+    }, {
+        path: '/release',
+        component: Release
+    }, {
+        path: '/shopcart',
+        component: Shopcart
+    }, {
+        path: '/user',
+        component: Mine
+    },
     {
-        path:'/home',
-        component:Home
-    },{
-        path:'/sort',
-        component:Sort
-    },{
-        path:'/release',
-        component:Release
-    },{
-        path:'/shopcart',
-        component:Shopcart
-    },{
-        path:'/user',
-        component:Mine
+        path: '/login',
+        component: Login
+    },
+    {
+        path: '/register',
+        component: Register
     }
 ]
 
