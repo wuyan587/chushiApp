e6618c7a311e67701d180e654c3cddea6feca07d
muhongjiu <1612930123@qq.com>
2019-12-27 14:37:40 +0800

    二次提交

---
+++ src/views/classify/Floor.vue
@@ -0,0 +1,80 @@
+<template>
+   <div class="floor">
+     <div v-for="(item,index) of floorsData" :key="index">
+              <h3>{{item.name}}</h3>
+     <ul>
+           <li v-for="ele of item.list" :key="ele.api_cid">
+             <router-link 
+             :to="{
+                 name:'goodslist',
+                 params:{
+                  id:ele.api_cid   
+                 }
+                 
+             }">
+                 <img :src="ele.img" alt="">
+                <span>{{ele.name}}</span>
+              </router-link>
+          </li>
+     </ul>
+     </div>
+   </div>
+</template>
+<script>
+export default {
+    // data(){
+    //     return {
+    //         floorsData:[]
+    //     }
+    // },
+    props:['floors'],
+    computed:{
+        floorsData(){
+            return this.floors&&this.floors.floors
+        }
+    },
+    // mounted(){
+// this.floorsData=this.floors.floors
+    
+    // }
+
+
+    
+}
+</script>
+<style lang="stylus" scoped>
+.floor
+ padding .1rem
+ div
+  margin-bottom .4rem
+  h3
+   font-size .16rem
+   font-weight bold
+   margin-bottom .2rem
+  ul
+   display flex
+   flex-wrap wrap
+   li
+    width 33.33%
+    a
+     display flex
+     flex-direction column
+     justify-content center
+     img 
+      height .65rem
+      width .65rem
+      margin-bottom .1rem
+     span 
+      color #696969
+      font-size .12rem
+      text-align center 
+      
+     
+     
+    
+ 
+
+  
+  
+ 
+</style>
