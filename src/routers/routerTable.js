27f9ab5db585e85e65ecafcf2986b74e230d181a
wuyan <751753090@qq.com>
2019-12-28 14:26:05 +0800

    发布

--- src/routers/routerTable.js
+++ src/routers/routerTable.js
@@ -1,8 +1,8 @@
-const Home=()=>import(/* 首页 */ '../views/home/index.vue')
-const Sort=()=>import(/* 分类 */ '../views/sort/index.vue')
-const Release=()=>import(/* 发布 */ '../views/release/index.vue')
-const Shopcart=()=>import(/* 货单 */ '../views/shopcart/index.vue')
-const Mine=()=>import(/* 我的 */ '../views/mine/index.vue')
+const Home=()=>import(/* 首页 */ 'views/home/index.vue')
+const Sort=()=>import(/* 分类 */ 'views/sort/index.vue')
+const Release=()=>import(/* 发布 */ 'views/release/index.vue')
+const Shopcart=()=>import(/* 货单 */ 'views/shopcart/index.vue')
+const Mine=()=>import(/* 我的 */ 'views/mine/index.vue')
 
 const routerTable=[
     {
