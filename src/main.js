67b56e657fd1a6b0b488d4608e859f4382ff4f59
wuyan <751753090@qq.com>
2019-12-31 08:34:31 +0800

    vuex‘
    git commit -m vuex‘

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routers'
+import store from './store'
 import {
     Popup,
     Search,
@@ -77,5 +78,6 @@ Vue.config.productionTip = false
 
 new Vue({
     router,
+    store,
     render: h => h(App),
 }).$mount('#app')
